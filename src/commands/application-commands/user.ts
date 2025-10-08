51cd858cce44d8044a172ac9be1b56ac993cd04a
ThisPythonJS <thispythonjs@gmail.com>
2025-10-07 22:24:24 -0300

    fix, style, refactor: changing links, texts and interactions.

--- src/commands/application-commands/user.ts
+++ src/commands/application-commands/user.ts
@@ -6,29 +6,31 @@ import { EmbedBuilder } from "discord.js";
 export default {
 	name: "user-info",
 	async run(client, interaction) {
+		await interaction.deferReply();
+		
 		const user = interaction.options.getUser("user") || interaction.user;
 		const userDb = await userSchema.findById(user.id);
 
-		if (!userDb) return interaction.reply("❌ Usuário não encontrado no banco de dados.");
+		if (!userDb) return interaction.reply("❌ Usuário não encontrado no banco de dados.\n🔹 Registre-se aqui: https://simo-botlist.vercel.app/login");
 
 		const userBots = await botSchema.find({
 			owner_id: user.id
 		});
 
 		const embed = new EmbedBuilder()
-			.setColor(0x0000ff)
-			.setTitle("Informações do usuário")
+			.setColor("#FFFFFF")
+			.setDescription(`## 👤 Informações do Usuário`)
 			.setThumbnail(user.displayAvatarURL())
-			.setImage(userDb.banner)
+			.setImage(userDb.banner ?? null)
 			.addFields({
 				name: "Usuário",
-				value: `**${userDb.username}** (\`${userDb._id}\`)`,
+				value: `**${userDb.username}**\n-# ╰ \`${userDb._id}\``,
 			}, {
 				name: "Biografia",
 				value: userDb?.bio ? userDb.bio : "Nenhuma biografia definida",
 			}, {
-				name: "Bots",
-				value: userBots.length ? userBots.map((bot) => `[${bot.name}](https://bombadeagua.vercel.app/bot/${bot._id})`).join(", ") : "Nenhum bot adicionado",
+				name: "Aplicações",
+				value: userBots.length ? userBots.map((bot) => `[${bot.name}](https://simo-botlist.vercel.app/bot/${bot._id})`).join(", ") : "Nenhum bot adicionado",
 			});
 
 		console.log("banner_url" in userDb);
@@ -37,7 +39,7 @@ export default {
 			embed.setImage(userDb.banner_url);
 		}
 
-		return interaction.reply({
+		return interaction.editReply({
 			embeds: [embed]
 		});
 	}
