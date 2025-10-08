bb7acb81c43e99e78f5137259bbcf1afbf60ae51
ThisPythonJS <thispythonjs@gmail.com>
2025-10-07 22:26:24 -0300

    refactor & style: text change and interaction response

--- src/commands/application-commands/ping.ts
+++ src/commands/application-commands/ping.ts
@@ -3,16 +3,18 @@ import type { ApplicationCommandStructure } from "../../types";
 export default {
     name: 'ping',
     async run(client, interaction) {
+        await interaction.deferReply();
+        
         const targetShard: number = interaction.options.getInteger('shard') ?? 0;
         const shard = client.ws.shards.get(targetShard);
 
         if (!shard) return interaction.reply({
-            content: `Shard ${targetShard} não foi encontrado`,
+            content: `🫣 Shard ${targetShard} não foi encontrado`,
             flags: "Ephemeral"
         });
 
-        return interaction.reply({
-            content: `Ping: **${shard.ping}**`
+        return interaction.editReply({
+            content: `🏓 Ping: **${shard.ping}ms**`
         });
     }
 } as ApplicationCommandStructure;
