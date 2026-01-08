f81c1bafd8070bfdcb3c3572ec2409892e73b3ac
ThisPythonJS <thispythonjs@gmail.com>
2026-01-08 15:12:39 -0300

    Arrumando o problema do array

--- src/events/reactionChannel.ts
+++ src/events/reactionChannel.ts
@@ -6,12 +6,12 @@ const REACTION_CHANNEL_IDS = ["1458587600511303804", "1458587698737709259"];
 client.on('messageCreate', async (message: Message): Promise<void> => {
     if (message.author.bot) return;
     
-    if (!message.channelId.includes(`${REACTION_CHANNEL_IDS}`)) return;
+    if (!REACTION_CHANNEL_IDS.includes(message.channelId)) return;
     
     if (message.channel.type !== ChannelType.GuildText) return;
       
-        await message.react('1458870534539317341');
-        await message.react('1458870522258657393');
-        
-        console.log(`Reação criada na mensagem do ${message.author.tag} (messageID: ${message.id})`);
-})
+    await message.react('1458870534539317341');
+    await message.react('1458870522258657393');
+    
+    console.log(`Reação criada na mensagem do ${message.author.tag} (messageID: ${message.id})`);
+});
